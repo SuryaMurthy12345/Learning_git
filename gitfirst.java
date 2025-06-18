32ba6aca5f1c9fd892ef60536d5e7502e5f49089
Surya Murthy <suryamurthy760@gmail.com>
2025-06-18 08:28:52 +0530

    My first committ

---
+++ gitfirst.java
@@ -0,0 +1,7 @@
+public class gitfirst{
+    public static void main(String[] args) {
+        // version control means , power of going back to any previous verion
+
+        //git is a distrubuted version control system, we can have local copy and remote copy
+    }
+}
